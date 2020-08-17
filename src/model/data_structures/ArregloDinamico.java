86eeef46809a5b182a240c4f4e8373847c2fc308
mateomp999 <m.martienzp@uniandes.edu.co>
2020-08-16 20:33:00 -0500

    version 1 taller 0

--- src/model/data_structures/ArregloDinamico.java
+++ src/model/data_structures/ArregloDinamico.java
@@ -1,5 +1,9 @@
 package model.data_structures;
 
+import java.lang.reflect.Array;
+
+import javax.swing.Box;
+
 /**
  * 2019-01-23
  * Estructura de Datos Arreglo Dinamico de Strings.
@@ -7,7 +11,8 @@ package model.data_structures;
  * @author Fernando De la Rosa
  *
  */
-public class ArregloDinamico implements IArregloDinamico {
+
+public class ArregloDinamico  <T extends Comparable<T>> implements IArregloDinamico<T> {
 		/**
 		 * Capacidad maxima del arreglo
 		 */
@@ -19,26 +24,27 @@ public class ArregloDinamico implements IArregloDinamico {
         /**
          * Arreglo de elementos de tamaNo maximo
          */
-        private String elementos[ ];
-
+        private Object elementos[];
+        
         /**
          * Construir un arreglo con la capacidad maxima inicial.
          * @param max Capacidad maxima inicial
          */
 		public ArregloDinamico( int max )
         {
-               elementos = new String[max];
+               elementos =  new Object[max];
                tamanoMax = max;
                tamanoAct = 0;
+               
         }
         
-		public void agregar( String dato )
+		public void agregar( T dato )
         {
                if ( tamanoAct == tamanoMax )
                {  // caso de arreglo lleno (aumentar tamaNo)
                     tamanoMax = 2 * tamanoMax;
-                    String [ ] copia = elementos;
-                    elementos = new String[tamanoMax];
+                    Object [ ] copia = elementos;
+                    elementos = new Object [tamanoMax]; 
                     for ( int i = 0; i < tamanoAct; i++)
                     {
                      	 elementos[i] = copia[i];
@@ -57,21 +63,60 @@ public class ArregloDinamico implements IArregloDinamico {
 			return tamanoAct;
 		}
 
-		public String darElemento(int i) {
+		public  T darElemento(int j) {
 			// TODO implementar
-			return null;
+	
+			return (T) elementos[j] ;
 		}
 
-		public String buscar(String dato) {
-			// TODO implementar
-			// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
-			return null;
-		}
-
-		public String eliminar(String dato) {
+		public <T extends Comparable<T>> T buscar(T dato) {
 			// TODO implementar
 			// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
-			return null;
+			T respuesta=null;
+			boolean parar=false;
+            for ( int i = 0; i < tamanoAct && parar==false; i++)
+            {
+            	T objeto= (T) elementos[i];
+             	 if (  dato.compareTo(objeto)==0)
+             	 {
+             		 respuesta=objeto;
+             		parar=true;
+             	 }
+            } 
+			return respuesta;
 		}
 
+		public <T extends Comparable<T>> T eliminar(T dato) {
+			// TODO implementar
+			// Recomendacion: Usar el criterio de comparacion natural (metodo compareTo()) definido en Strings.
+			T respuesta=null;
+			boolean parar=false;
+            for ( int i = 0; i < tamanoAct && parar==false; i++)
+            {
+            	T objeto= (T) elementos[i];
+             	 if (  dato.compareTo(objeto)==0)
+             	 {
+             		respuesta=objeto;
+             		
+             		for (int j=i;j< tamanoAct;j++)
+             		{
+             			if (j!=tamanoAct-1)
+             				{
+             				elementos[j]=elementos[j+1];
+             				}
+             			else
+             			{
+             				elementos[j]=null;
+             			}
+             		}
+             		
+             		parar=true;
+             		tamanoAct=tamanoAct-1;
+             	 }
+            } 
+			return respuesta;
+		
+		}
+
+		
 }
---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module T0_202020 {
+	exports test.data_structures;
+	exports controller;
+	exports view;
+	exports test.logic;
+	exports model.logic;
+	exports main;
+	exports model.data_structures;
+
+	requires junit;
+	requires java.desktop;
+}
